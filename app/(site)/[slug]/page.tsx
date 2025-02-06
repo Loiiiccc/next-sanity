f8d93571e4c32daf39a3ea855911a3ed5783da83
Loïc Ayassou <loicaya1@gmail.com>
2025-02-06 06:18:35 -0500

    fixing params type

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -2,11 +2,11 @@ import { getPage } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
 
 type Props = {
-    params: {slug: string}
+    params: Promise<{slug: string}>
 }
 
 export default async function Page({params}: Props) {
-    const page = await getPage(params.slug);
+    const page = await getPage((await params).slug);
     return (
         <div>
             <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-7xl font-bold">{page.title}</h1>
